4ce849355973469c696a215cd054ecf1111e0e48
David Jacquet <jacquet.david@gmail.com>
2018-11-13 18:26:01 +0100

    Added test of car case ...

--- deep_serializer/tests/test_des.py
+++ deep_serializer/tests/test_des.py
@@ -1,7 +1,9 @@
-import datetime
-import typing
 import unittest
 
+import datetime
+import typing
+
+from deep_serializer.aux.example import Car
 from deep_serializer.des import _DecAux
 
 try:
@@ -20,3 +22,26 @@ class TestStringMethods(unittest.TestCase):
     def test_conv(self):
         self.__aux(typing.Union[int, float, str], '7', '7')
         self.__aux(datetime.datetime, '2015-01-01T00:00:00', datetime.datetime(2015, 1, 1))
+
+    def test_car(self):
+        car_dict = {
+            'color': 'blue',
+            'model': 'Volvo',
+            'owner': {'age': '22', 'name': 'Bob'},
+            'passengers': [{'age': 20, 'name': 'Alice'}, {'age': 19, 'name': 'Joe'}],
+            'registration_date': '2015-01-01 12:00.00',
+            'miles': {'2015-01-01 12:00.00': 0, '2016-01-01 12:00.00': '1000'}
+        }
+        expected_result = {'color': 'blue',
+                           'miles': {datetime.datetime(2015, 1, 1, 12, 0): 0.0,
+                                     datetime.datetime(2016, 1, 1, 12, 0): 1000.0},
+                           'model': 'Volvo',
+                           'owner': {'age': 22, 'name': 'Bob'},
+                           'passengers': [{'age': 20, 'name': 'Alice'}, {'age': 19, 'name': 'Joe'}],
+                           'registration_date': datetime.datetime(2015, 1, 1, 12, 0)}
+
+        car = Car(**car_dict)
+        self.assertTrue(expected_result == car.todict())
+        self.assertDictEqual(expected_result, car.todict())
+        car.registration_date += datetime.timedelta(days=12)
+        self.assertFalse(expected_result == car.todict())
